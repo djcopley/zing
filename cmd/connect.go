9eb15dd4b1229381fed785437c3ce7b29207de13
Daniel Copley <djcopley@proton.me>
2025-06-24 23:53:06 -0500

    add connect command

--- cmd/connect.go
+++ cmd/connect.go
@@ -1,14 +1,25 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
+)
+
+var (
+	host string
+	port int
 )
 
 func init() {
 	rootCmd.AddCommand(connectCommand)
+	connectCommand.Flags().StringVarP(&host, "host", "H", "localhost", "Host to connect to")
+	connectCommand.Flags().IntVarP(&port, "port", "P", 8080, "Port to connect to")
 }
 
 var connectCommand = &cobra.Command{
 	Use:   "connect",
 	Short: "Connect to the server",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Connecting to %s:%d\n", host, port)
+	},
 }
