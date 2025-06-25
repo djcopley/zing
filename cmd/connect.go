07f3c70766dadb050774f6ba791f677078a7dda3
Daniel Copley <djcopley@proton.me>
2025-06-25 17:27:40 -0500

    add test connect command

--- cmd/connect.go
+++ cmd/connect.go
@@ -1,7 +1,12 @@
 package cmd
 
 import (
+	"context"
+	"fmt"
+	"github.com/djcopley/zing/api"
 	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"log"
 )
 
@@ -13,6 +18,24 @@ var connectCommand = &cobra.Command{
 	Use:   "connect",
 	Short: "Connect to the server",
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Printf("connecting to %s:%d\n", host, port)
+		addr := fmt.Sprintf("%s:%d", host, port)
+		conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			log.Fatalf("failed to connect to server: %s\n", err)
+		}
+		defer conn.Close()
+		c := api.NewZingClient(conn)
+
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		r, err := c.GetMessages(ctx, &api.GetMessagesRequest{})
+		if err != nil {
+			log.Fatalf("failed to connect to server: %s\n", err)
+		}
+		res, err := r.Recv()
+		if err != nil {
+			log.Fatalf("failed to receive message from server: %s\n", err)
+		}
+		log.Println(res)
 	},
 }
